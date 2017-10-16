bdfe53e4913507e16716f76fd61fe02e8954c339
sunrise <thampy@gmail.com>
2017-10-15 17:21:41 -0700

    made some changes

--- t-blackjack.py
+++ t-blackjack.py
@@ -6,4 +6,7 @@ if len(sys.argv) - 1 != 1:
     print 'Usage: ' + sys.argv[0] + ' <test-file.csv>'
     sys.exit(0)
 
+with open(sys.argv[1], 'r') as infile:
+    for line in infile:
+        rows = line.rstrip().split(',')
 
